a98cb78d33edd3ac7278ab24d5930b03f9ff5b7a
Sergey Litvinov <slitvinov@gmail.com>
2021-04-21 19:01:31 +0200

    minor

--- src/lqr.c
+++ src/lqr.c
@@ -5,6 +5,7 @@ static double s;
 static double x;
 static double v;
 static double F;
+static int k;
 
 static double reward(void);
 static double action(void);
@@ -12,7 +13,6 @@ static double action(void);
 int
 main(int agrc, char **argv)
 {
-    int k;
     int L;
     double r;
     double R;
@@ -26,7 +26,7 @@ main(int agrc, char **argv)
         F = action();
         r = reward();
         R += r;
-        printf("%12.6g %12.6g\n", x, R);
+        printf("%12.6g %12.6g %12.6g\n", x, v, R);
         if (k == L - 1)
           break;
         x += v;
@@ -41,7 +41,29 @@ reward(void)
 }
 
 static double
-action(void)
+action0(void)
 {
   return -0.196078431372549;
+}
+
+static double
+action1(void)
+{
+  double f[] = {-0.4705882352941176,
+                0.08823529411764705,
+                0.1176470588235294,
+                0};
+  return f[k];
+}
+
+static double
+action2(void)
+{
+  return -0.2272025184027502 * x;
+}
+
+static double
+action(void)
+{
+  return -0.2534710463934982 * x + 0.04087719298245614;
 }

3d87394b48f41febc5da9b3794246df43524ac50
Sergey Litvinov <slitvinov@gmail.com>
2021-04-21 20:48:11 +0200

    dlog

--- src/lqr_reinforce.c
+++ src/lqr_reinforce.c
@@ -4,18 +4,21 @@
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
 
+enum {dim = 2};
 static double s;
 static int L;
 static double x;
 static double v;
 static double F;
 static int k;
-static double t[3];
+static double t[dim];
 static gsl_rng *r;
+static double z[2*dim];
 
 static double reward(void);
 static double action(void);
-static void sample(void);
+static void sample(const double*, double *);
+static void dlog(const double*, const double*, double*);
 
 static double
 episod(void)
@@ -42,32 +45,37 @@ int
 main(int agrc, char **argv)
 {
     double alpha;
-    double d[2];
-    double Rm;
-    double Rp;
-    double sigma;
-    double t0[2];
+    double R;
+    double aux;
     int c;
     int i;
+    int j;
+    int n;
+    const gsl_rng_type *T;
+    static double g[2*dim];
 
+    gsl_rng_env_setup();
+    T = gsl_rng_default;
+    r = gsl_rng_alloc(T);    
     L = 5;
     s = 1.0;
-    t[0] = t[1] = 0;
-    sigma = 1e-3;
-    alpha = 1e-2;
-    for (i = 0; i < 300; i++) {
-        for (c = 0; c < 2; c++) {
-            memcpy(t, t0, sizeof t0);
-            t[c] = t0[c] + sigma;
-            Rp = episod();
-            memcpy(t, t0, sizeof t0);
-            t[c] = t0[c] - sigma;
-            Rm = episod();
-            d[c] = (Rp - Rm) / (2 * sigma);
-        }
-        for (c = 0; c < 2; c++)
-            t0[c] -= alpha * d[c];
-        printf("%12.6g %12.6g %12.6g\n", Rm, t0[0], t0[1]);
+    n = 1000;
+    z[0] = -0.1; z[1] = 0.001;
+    z[2] = 0; z[3] = 0.001;
+    alpha = 1e-7;
+    for (i = 0; i < 100000; i++) {
+      for (c = 0; c < 2 * dim; c++)
+        g[c] = 0;
+      for (j = 0; j < n; j++) {
+        sample(z, t);
+        R = episod();
+        aux = alpha * R;
+        dlog(t, z, g);
+      }
+      for (c = 0; c < 2 * dim; c++)
+        z[c] -= aux * g[c] / n;
+      if (i % 10000 == 0)        
+        printf("%12.6g %12.6g %12.6g\n", R, z[0], z[2]);
     }
 }
 
@@ -83,3 +91,39 @@ action(void)
     return t[0] * x + t[1];
 }
 
+static void
+sample(const double *z, double *t)
+{
+  double m;
+  double s;
+
+  m = z[0];
+  s = z[1];
+  t[0] = gsl_ran_gaussian(r, s) + m;
+
+  m = z[2];
+  s = z[3];
+  t[1] = gsl_ran_gaussian(r, s) + m;  
+}
+
+static void
+dlog(const double *t, const double *z, double *g)
+{
+  double m;
+  double s;
+  double x;
+
+  m = z[0];
+  s = z[1];
+  x = t[0];
+  g[0] += (2 * (x - m)) / (s * s);
+  g[1] +=
+    (2 * (x * x) - (4 * m * x + (s * s)) + 2 * (m * m)) / (s * s * s);
+
+  m = z[2];
+  s = z[3];
+  x = t[1];
+  g[2] += (2 * (x - m)) / (s * s);
+  g[3] +=
+    (2 * (x * x) - (4 * m * x + (s * s)) + 2 * (m * m)) / (s * s * s);
+}
